1320c938e97659d6208544fff93683c0d7ebf17a
Gleydson Fabricio <gleydsonfabricio3@gmail.com>
2025-05-11 17:42:46 -0300

    created controller and services paths

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,20 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+class UserController {
+
+    async create(request: FastifyRequest, reply: FastifyReply){
+
+
+    }
+
+    async delete(request: FastifyRequest, reply: FastifyReply){
+
+    }
+
+    async list(request: FastifyRequest, reply: FastifyReply){
+
+    }
+
+}
+
+export { UserController }
