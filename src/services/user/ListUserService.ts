1d1ed6064b42ff16feaa52c18def92409092fde5
Eduardo <eduardosume082@gmail.com>
2025-05-15 15:54:13 -0300

    feat:list and delete users

--- src/services/user/ListUserService.ts
+++ src/services/user/ListUserService.ts
@@ -1,9 +1,11 @@
+import prismaClient from "../../prisma"
+
 class ListUserService{
 
     async execute(){
-        
+        const users = await prismaClient.user.findMany()
+        return users
     }
-    
 }
 
 export {ListUserService}
